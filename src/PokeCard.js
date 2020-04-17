1f1fc4e06460151a45df43403585ca31f43a9f85
Michael Mallowe <mmallowe@gmail.com>
2020-04-16 19:29:45 -0700

    better images & function to return 3 digit ids

--- src/PokeCard.js
+++ src/PokeCard.js
@@ -3,8 +3,13 @@ import './PokeCard.css'
 
 class PokeCard extends Component {
     render() {      
-        const POKE_API ="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/";
-        let imgSrc = `${POKE_API}${this.props.id}.png`  
+        // const POKE_API ="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/";
+        // let imgSrc = `${POKE_API}${this.props.id}.png`  
+        const POKE_API ="https://assets.pokemon.com/assets/cms2/img/pokedex/detail/";
+
+        let padToThree = (number) => (number <= 999 ? `00${number}`.slice(-3): number)
+
+        let imgSrc = `${POKE_API}${padToThree(this.props.id)}.png`  
         return (
             <div className="PokeCard">
                 <h2>{this.props.name}</h2>
