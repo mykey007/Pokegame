3549d8b055f5a85239437b4a0f89e65d1707a304
Michael Mallowe <mmallowe@gmail.com>
2020-04-16 19:07:13 -0700

    make pokedex and loop through our array

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import logo from './logo.svg';
 import Cat from './Cat';
 import './App.css';
-import PokeCard from './PokeCard';
+import Pokedex from './Pokedex';
 
 function App() {
   return (
@@ -12,28 +12,7 @@ function App() {
         <h1>Pokedex</h1>
       </header>
       <section className="flexer">
-        <PokeCard id={4} name="Charmander" type="fire" exp={62}/>
-
-        {/* <PokeCard
-          name=""
-          image=""
-          type=""
-          exp=""
-        />
-
-        <PokeCard
-          name=""
-          image=""
-          type=""
-          exp=""
-        />
-
-        <PokeCard
-          name=""
-          image=""
-          type=""
-          exp=""
-        /> */}
+        <Pokedex/>
       </section>
 
     </div>
